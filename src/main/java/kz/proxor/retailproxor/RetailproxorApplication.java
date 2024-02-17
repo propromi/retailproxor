15bff40dbfde7e7161b6639ed834130e83abc27b
propromi <propromig@gmail.com>
2024-02-17 15:52:08 +0600

    init commit

---
+++ src/main/java/kz/proxor/retailproxor/RetailproxorApplication.java
@@ -0,0 +1,13 @@
+package kz.proxor.retailproxor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailproxorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailproxorApplication.class, args);
+	}
+
+}
